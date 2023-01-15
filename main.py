d9f7e17e28ce59fb6842314f86523d6402846996
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2023-01-15 20:57:05 +0200

    ver1

--- main.py
+++ main.py
@@ -161,23 +161,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sentences = [
-        "This first sentence.",
-        "This is the second.",
-        "This is the third sentence.",
-        "This the fourth sentence.",
-        "This sentence.",
-        "This.",
-        " the second sentence.",
-        "This is the third sentence.",
-        "This is the fourth sentence.",
-        "fth sentence.",
-        "This is the first sentence.",
-        "This second.",
-        "This third sentence.",
-        "This fourth sentence.",
-        "This is the fifth sentence.",
+        "Deep learning (also known as deep structured learning) is part of a broader family of machine learning methods based on artificial neural networks with representation learning. Learning can be supervised, semi-supervised or unsupervised. Deep-learning architectures such as deep neural networks, deep belief networks, deep reinforcement learning, recurrent neural networks and convolutional neural networks have been applied to fields including computer vision, speech recognition, natural language processing, machine translation, bioinformatics, drug design, medical image analysis, material inspection and board game programs, where they have produced results comparable to and in some cases surpassing human expert performance. Artificial neural networks (ANNs) were inspired by information processing and distributed communication nodes in biological systems. ANNs have various differences from biological brains. Specifically, neural networks tend to be static and symbolic, while the biological brain of most living organisms is dynamic (plastic) and analogue. The adjective deep in deep learning refers to the use of multiple layers in the network. Early work showed that a linear perceptron cannot be a universal classifier, but that a network with a nonpolynomial activation function with one hidden layer of unbounded width can. Deep learning is a modern variation which is concerned with an unbounded number of layers of bounded size, which permits practical application and optimized implementation, while retaining theoretical universality under mild conditions. In deep learning the layers are also permitted to be heterogeneous and to deviate widely from biologically informed connectionist models, for the sake of efficiency, trainability and understandability, whence the structured part."
     ]
-    summary_length = 3  # Number of sentences to include in the summary
+    summary_length =1  # Number of sentences to include in the summary
     summary, ranked_sentences = lexrank(sentences, summary_length)
     print(summary)
     visualize_lexrank(ranked_sentences)
@@ -186,8 +172,7 @@ if __name__ == '__main__':
     # print(ranked_sentences)
 
 
-    document = "This first sentence.This is the second.This is the third sentence.This the fourth sentence.This sentence.This. the second sentence.This is the third sentence.This is the fourth sentence.fth sentence.",
-    document = '.'.join(document)
+    document ="Deep learning (also known as deep structured learning) is part of a broader family of machine learning methods based on artificial neural networks with representation learning. Learning can be supervised, semi-supervised or unsupervised. Deep-learning architectures such as deep neural networks, deep belief networks, deep reinforcement learning, recurrent neural networks and convolutional neural networks have been applied to fields including computer vision, speech recognition, natural language processing, machine translation, bioinformatics, drug design, medical image analysis, material inspection and board game programs, where they have produced results comparable to and in some cases surpassing human expert performance. Artificial neural networks (ANNs) were inspired by information processing and distributed communication nodes in biological systems. ANNs have various differences from biological brains. Specifically, neural networks tend to be static and symbolic, while the biological brain of most living organisms is dynamic (plastic) and analogue. The adjective deep in deep learning refers to the use of multiple layers in the network. Early work showed that a linear perceptron cannot be a universal classifier, but that a network with a nonpolynomial activation function with one hidden layer of unbounded width can. Deep learning is a modern variation which is concerned with an unbounded number of layers of bounded size, which permits practical application and optimized implementation, while retaining theoretical universality under mild conditions. In deep learning the layers are also permitted to be heterogeneous and to deviate widely from biologically informed connectionist models, for the sake of efficiency, trainability and understandability, whence the structured part."
     show_textrank_graph(document)
     print(textrank(document))
 
